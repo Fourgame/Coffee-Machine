1f9f47456c990357fa09396f3b0840239bdda57d
Fourgame <loukfour@gmail.com>
2023-03-12 15:27:54 +0700

    add cashout number 1

---
+++ src/main/java/com/coffeemachine/coffeemachineguifx/CoffeeCashOut.java
@@ -0,0 +1,10 @@
+package com.coffeemachine.coffeemachineguifx;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CoffeeCashOut {
+
+}
